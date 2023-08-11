a64c29273218e131a75780854a83aa98f176e117
kevin <kevincolmenaresm@gmail.com>
2023-08-11 16:17:12 -0400

    Acomodando el index y creando un Hook

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,32 @@
+import styled from "@emotion/styled";
+import { useSelectMonedas } from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
+
+const InputSubmit = styled.input`
+  background-color: #9497ff;
+  border: none;
+  width: 100%;
+  padding: 10px;
+  color: #fff;
+  font-weight: 700;
+  text-transform: uppercase;
+  font-size: 20px;
+  border-radius: 5px;
+  transition: background-color 0.3s ease;
+  margin-top: 30px;
+  &:hover {
+    background-color: #7a7dfe;
+    cursor: pointer;
+  }
+`;
+
+export const Formulario = () => {
+  const [moneda, SelectMonedas] = useSelectMonedas("Elige tu moneda", monedas);
+
+  return (
+    <form>
+      <SelectMonedas />
+      <InputSubmit type="submit" value="cotizar" />
+    </form>
+  );
+};
---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react-refresh/only-export-components */
+import styled from "@emotion/styled";
+import { useState } from "react";
+
+const Label = styled.label`
+    font-family: "Noto Sans mono", monospace;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 24px;
+    margin: 15px 0
+    display: block;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  display: block;
+  padding: 12px;
+  margin-top: 10px;
+  font-size: 18px;
+  border-radius: 10px;
+`;
+
+/**
+ *
+ * @param {string} label
+ * @param {Array<string>} opciones
+ * @returns
+ */
+export const useSelectMonedas = (label, opciones) => {
+
+  const [state, setState] = useState("");
+
+  const SelectMonedas = () => (
+    <>
+      <Label htmlFor="">{label}</Label>
+      <Select value={state} onChange={(e) => setState(e.target.value)}>
+        <option value="">- Seleccione -</option>
+        {opciones.map((opcion) => (
+          <option key={opcion.id} value={opcion.id}>
+            {opcion.nombre}
+          </option>
+        ))}
+      </Select>
+    </>
+  );
+
+  return [state, SelectMonedas];
+};
